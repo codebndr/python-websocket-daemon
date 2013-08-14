761d1d6a1b58779257b85d2ac412840a845bcc8a
Vasilis Georgitzikis <tzikis@gmail.com>
2013-08-14 14:40:23 +0300

    added windows drivers and driver installation

--- myserver.py
+++ myserver.py
@@ -29,9 +29,17 @@ logging.basicConfig(filename='app.log', level=logging.INFO)
 def install_drivers_osx():
 	os.system("""osascript -e 'do shell script "/usr/sbin/installer -pkg drivers/Darwin/FTDIUSBSerialDriver_10_4_10_5_10_6_10_7.mpkg/ -target /" with administrator privileges'""")	
 
+def install_drivers_windows():
+        if platform.architecture()[0] == "32bit":
+                os.system(os.getcwd() + "/drivers/Windows/dpinst-x86.exe")
+        else:
+                os.system(os.getcwd() + "/drivers/Windows/dpinst-amd64.exe")
+
 def install_drivers():
 	if platform.system() == "Darwin":
 		install_drivers_osx()
+	elif platform.system() == "Windows":
+                install_drivers_windows()
 
 def do_install_drivers():
 	# Create a thread as follows
