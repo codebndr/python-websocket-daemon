d1411087ec4f47f53130c55452c682381a4bf92d
Vasileios Georgitzikis <tzikis@gmail.com>
2013-08-14 14:23:26 -0700

    added driver check & installation on osx and auto-refresh of port list

--- myserver.py
+++ myserver.py
@@ -26,6 +26,42 @@ import platform
 
 logging.basicConfig(filename='app.log', level=logging.INFO)
 
+def install_drivers():
+	os.system("""osascript -e 'do shell script "/usr/sbin/installer -pkg /Users/iluvatar/Documents/Projects/codebender/FTDIUSBSerialDriver_10_4_10_5_10_6_10_7.mpkg/ -target /" with administrator privileges'""")
+	
+
+def do_install_drivers():
+	# Create a thread as follows
+	try:
+		thread.start_new_thread(install_drivers, ())
+	except:
+		print "Error: unable to start thread"
+	
+def check_drivers(websocket):
+	if(os.path.exists("/var/db/receipts/com.FTDI.ftdiusbserialdriverinstaller.FTDIUSBSerialDriver-2.pkg.bom") and os.path.exists("/var/db/receipts/com.FTDI.ftdiusbserialdriverinstaller.FTDIUSBSerialDriver-2.pkg.plist") and os.path.exists("/var/db/receipts/com.FTDI.ftdiusbserialdriverinstaller.postflight.pkg.bom") and os.path.exists("/var/db/receipts/com.FTDI.ftdiusbserialdriverinstaller.postflight.pkg.plist") and os.path.exists("/var/db/receipts/com.FTDI.ftdiusbserialdriverinstaller.preflight.pkg.bom")):
+		websocket.sendMessage(json.dumps({"type":"check_drivers","installed":True}))
+	else:
+		websocket.sendMessage(json.dumps({"type":"check_drivers","installed":False}))
+
+
+def flash_arduino(cpu, ptc, prt, bad, binary):
+	bash_shell_cmd = "./avrdudes/" + platform.system() + "/avrdude"
+	bash_shell_cnf = " -C./avrdudes/" + platform.system() + "/avrdude.conf"
+	bash_shell_cpu = " -p" + cpu
+	bash_shell_ptc = " -c" + ptc
+	bash_shell_prt = " -P" + prt
+	bash_shell_bad = " -b" + bad
+	binary = base64.b64decode(binary)
+	bin_file = tempfile.NamedTemporaryFile()
+	print "file name: ", bin_file.name
+	bin_file.write(binary)
+	bash_shell_file = " -Uflash:w:" + bin_file.name + ":i"
+	bash_shell = bash_shell_cmd + bash_shell_cnf + " -v -v -v -v" + bash_shell_cpu + bash_shell_ptc + bash_shell_prt + bash_shell_bad + " -D" + bash_shell_file
+	print "Flashing: ", bash_shell
+	process = subprocess.Popen(bash_shell.split())
+	print "FLASHING!!!"
+	
+
 def check_ports():
 	list = list_ports.comports()
 
@@ -36,17 +72,24 @@ def check_ports():
 	return ports
 
 def update_ports(websocket):
+	print "Starting check for ports"
 	old_ports = []
 	while 1:
 		ports = check_ports()
 		if(ports != old_ports):
 			for port in ports:
 				print port
-			websocket.sendMessage(json.dumps({"type":"ports", "ports":ports}))
+			print "Sending Ports Message"
+			websocket.sendMessage("hello")
+			# websocket.sendMessage(json.dumps({"type":"ports", "ports":ports}))
 		old_ports = ports
-		time.sleep(500)
+		# time.sleep(0.05)
 
 class EchoServerProtocol(WebSocketServerProtocol):
+
+	def sendPorts(self):
+		ports = check_ports()
+		self.sendMessage(json.dumps({"type":"ports", "ports":ports}))
 
 	def onMessage(self, msg, binary):
 		logging.info("Received message: %s", msg)
@@ -56,37 +99,26 @@ class EchoServerProtocol(WebSocketServerProtocol):
 		if message["type"] == "version":
 			self.sendMessage(json.dumps({"type":"version", "version":"0.1"}) ,binary)
 		elif message["type"] == "flash":
-			bash_shell_cmd = "./avrdudes/" + platform.system() + "/avrdude"
-			bash_shell_cnf = " -C./avrdudes/" + platform.system() + "/avrdude.conf"
-			bash_shell_cpu = " -p" + message["cpu"]
-			bash_shell_ptc = " -c" + message["ptc"]
-			bash_shell_prt = " -P" + message["prt"]
-			bash_shell_bad = " -b" + message["bad"]
-			binary = base64.b64decode(message["binary"])
-			bin_file = tempfile.NamedTemporaryFile()
-			print "file name: ", bin_file.name
-			bin_file.write(binary)
-			bash_shell_file = " -Uflash:w:" + bin_file.name + ":i"
-			bash_shell = bash_shell_cmd + bash_shell_cnf + " -v -v -v -v" + bash_shell_cpu + bash_shell_ptc + bash_shell_prt + bash_shell_bad + " -D" + bash_shell_file
-			print "Flashing: ", bash_shell
-			process = subprocess.Popen(bash_shell.split())
-			print "FLASHING!!!"
+			flash_arduino(message["cpu"], message["ptc"], message["prt"], message["bad"], message["binary"])
 		elif message["type"] == "message":
 			print message["text"]
+		elif message["type"] == "list_ports":
+			reactor.callLater(1, self.sendPorts)
+		elif message["type"] == "check_drivers":
+			check_drivers(self)
+		elif message["type"] == "install_drivers":
+			do_install_drivers()
 		else:
 			self.sendMessage(msg, binary)
 
 	def onOpen(self):
 		self.sendMessage(json.dumps({"text":"Socket Opened"}));
-		ports = check_ports()
-		self.sendMessage(json.dumps({"type":"ports", "ports":ports}))
-		self.sendMessage(json.dumps({"type":"version", "version":"0.1"}))
 
-		# Create a thread as follows
-		try:
-			thread.start_new_thread(update_ports, (self, ))
-		except:
-			print "Error: unable to start thread"
+		# # Create a thread as follows
+		# try:
+		# 	thread.start_new_thread(update_ports, (self, ))
+		# except:
+		# 	print "Error: unable to start thread"
 
 log.startLogging(sys.stdout)
 
