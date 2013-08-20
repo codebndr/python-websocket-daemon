f5bfca6a636b655e62fae91e0e81c98ac0ae4d9e
Vasileios Georgitzikis <tzikis@gmail.com>
2013-08-19 17:14:41 -0700

    added web serialmonitor code

--- myserver.py
+++ myserver.py
@@ -6,6 +6,7 @@ import serial
 from serial.tools import list_ports
 
 import thread
+import threading
 
 #needed for py2exe
 import zope.interface
@@ -153,10 +154,9 @@ def update_ports(websocket):
 			for port in ports:
 				print port
 			print "Sending Ports Message"
-			websocket.sendMessage("hello")
-			# websocket.sendMessage(json.dumps({"type":"ports", "ports":ports}))
+			websocket.sendMessage(json.dumps({"type":"ports", "ports":ports}))
 		old_ports = ports
-		# time.sleep(0.05)
+		time.sleep(0.05)
 
 class EchoServerProtocol(WebSocketServerProtocol):
 
@@ -171,6 +171,8 @@ class EchoServerProtocol(WebSocketServerProtocol):
 		
 		if message["type"] == "version":
 			self.sendMessage(json.dumps({"type":"version", "version":"0.1"}) ,binary)
+		elif message["type"] == "ack":
+			self.sendMessage(msg, binary)
 		elif message["type"] == "flash":
 			if platform.system() == "Windows":
                                 message["prt"] = '\\\\.\\' + message["prt"]
@@ -189,11 +191,91 @@ class EchoServerProtocol(WebSocketServerProtocol):
 	def onOpen(self):
 		self.sendMessage(json.dumps({"text":"Socket Opened"}));
 
-		# # Create a thread as follows
-		# try:
-		# 	thread.start_new_thread(update_ports, (self, ))
-		# except:
-		# 	print "Error: unable to start thread"
+		# Create a thread as follows
+		try:
+			thread.start_new_thread(update_ports, (self, ))
+		except:
+			print "Error: unable to start thread"
+
+
+################ WEBSERIAL CODE
+
+class ReaderThread(threading.Thread, ):
+	"""Thread class with a stop() method. The thread itself has to check
+	regularly for the stopped() condition."""
+
+	def __init__(self, websocket_port, serial_port):
+		super(ReaderThread, self).__init__()
+		self._stop = threading.Event()
+		self.websocket_port = websocket_port
+		self.serial_port = serial_port
+
+	def stop(self):
+		self.run = False
+		self._stop.set()
+
+	def stopped(self):
+		return self._stop.isSet()
+
+	def run(self):
+		self.run = True
+		self.read()
+
+	def read(self):
+		"""loop and copy serial->console"""
+		try:
+			while self.run:
+				data = self.serial_port.read(10).decode("ascii")
+				if(data != ""):
+					sys.stdout.write(data)
+					sys.stdout.flush()
+					self.websocket_port.sendMessage(json.dumps({"type":"data", "data":data}))
+		except serial.SerialException, e:
+			raise
+
+def writer(serial_port, data):
+	serial_port.write(data)
+
+class WebserialProtocol(WebSocketServerProtocol):
+	def __init__(self):
+		self.myserial = None
+
+	def onMessage(self, msg, binary):
+		logging.info("Received message: %s", msg)
+		print "Received message: ", msg
+		message = json.loads(msg)
+
+		if message["type"] == "version":
+			self.sendMessage(json.dumps({"type":"version", "version":"0.1"}) ,binary)
+		elif message["type"] == "ack":
+			self.sendMessage(msg, binary)
+		elif message["type"] == "serial_connect":
+			print "Connect Message Received"
+			self.myserial = serial.Serial(message["port"], baudrate= message["baudrate"], timeout=0)
+			self.read_thread = ReaderThread(self, self.myserial)
+			self.read_thread.daemon = True
+			self.read_thread.start()
+		elif message["type"] == "serial_disconnect":
+			print "Disconnect Message Received"
+			if self.myserial != None:
+				oldserial = self.myserial
+				self.myserial = None
+				self.read_thread.stop()
+				self.read_thread.join()
+				oldserial.close()
+		elif message["type"] == "data":
+			if self.myserial != None:
+				writer(self.myserial, message["data"])
+			else:
+				print "Could not send data. No active serial connections"
+		else:
+			self.sendMessage(msg, binary)
+
+	def onOpen(self):
+		self.sendMessage(json.dumps({"text":"Socket Opened"}));
+
+
+################ END OF WEBSERIAL CODE
 
 log.startLogging(sys.stdout)
 
@@ -202,7 +284,14 @@ list = []
 # ?
 if __name__ == '__main__':
 
+	#initializing serial flashing etc utilities socket
 	factory = WebSocketServerFactory("ws://localhost:9000", debug = False)
 	factory.protocol = EchoServerProtocol
 	listenWS(factory)
+
+	#initializing webserial socket
+	factory2 = WebSocketServerFactory("ws://localhost:9001", debug = False)
+	factory2.protocol = WebserialProtocol
+	listenWS(factory2)
+
 	reactor.run()
