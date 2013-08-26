dd715d967632f21db8cbaf1985bd933fd3457d37
Vasilis Georgitzikis <tzikis@gmail.com>
2013-08-26 22:28:33 +0300

    improved standalone .exe setup script

--- setup-old.py
+++ setup-old.py
@@ -1,4 +1,17 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['myserver.py'])
+my_data_files = [('avrdudes/Windows', ['avrdudes/Windows/avrdude.conf']),
+                ('avrdudes/Windows', ['avrdudes/Windows/avrdude.exe']),
+                ('avrdudes/Windows', ['avrdudes/Windows/libusb0.dll']),
+                ('drivers/Windows', ['drivers/Windows/arduino.cat']),
+                ('drivers/Windows', ['drivers/Windows/arduino.inf']),
+                ('drivers/Windows', ['drivers/Windows/dpinst-x86.exe']),
+                ('drivers/Windows', ['drivers/Windows/dpinst-amd64.exe'])]
+
+
+
+setup(options = {"py2exe": {"compressed": 1, "bundle_files": 1} },
+      zipfile = None,
+      data_files = my_data_files,
+      console=['myserver.py'])
